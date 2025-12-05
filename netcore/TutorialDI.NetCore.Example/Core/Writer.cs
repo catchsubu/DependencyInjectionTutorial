c63e3b3039b4a6dd76c85923435a4cf6fc4b2805
Subrata Mohanta <subrata.mohanta@gmail.com>
2025-12-06 01:38:08 +0530

    docs: Update README to include .NET Core DI examples and platform comparison

---
+++ netcore/TutorialDI.NetCore.Example/Core/Writer.cs
@@ -0,0 +1,8 @@
+﻿namespace TutorialDI.NetCore.Example.Core
+{
+    public class Writer(IWritingInstrument writingInstrument)
+    {
+        public void WriteEssay(string essayTopic) =>
+            writingInstrument?.WriteEssay(essayTopic);
+    }
+}
---
+++ netcore/TutorialDI.NetCore.Example/Instrument/Pencil.cs
@@ -0,0 +1,10 @@
+﻿using TutorialDI.NetCore.Example.Core;
+
+namespace TutorialDI.NetCore.Example.Instrument
+{
+    public class Pencil : IWritingInstrument
+    {
+        public void WriteEssay(string essayTopic) =>
+            Console.WriteLine($"Writing an essay on \"{essayTopic}\" using a pencil.");
+    }
+}
